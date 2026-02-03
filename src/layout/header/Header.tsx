164a3607737ca08c71bc2acd160372d299000b3c
Erik <erikpersson0884@gmail.com>
2026-02-03 20:40:44 +0100

    updated firstpage

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -4,9 +4,11 @@ import erik from '../../assets/erik.jpg'
 const Header = () => {
     return (
         <header className='page-header'>
+            <div>
+                <h1>Erik Persson</h1>
+                <h2>Software engineering student</h2>
+            </div>
             <img src={erik} alt="picture of Erik Persson" />
-            <h1>Erik Persson</h1>
-            <h2>Software engineering student</h2>
         </header>   
     )
 }
