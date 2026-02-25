e64818aa2b4c91bce7f934201c7b639d2dabee42
Erik <erikpersson0884@gmail.com>
2026-02-25 21:18:43 +0100

    implemented sidebar correctly

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -8,14 +8,6 @@ import linkedin from '../../assets/icons/linkedin.svg'
 
 const Sidebar: FC = () => {
 
-    const links: { name: string, url: string }[] = [
-        { name: 'Professions', url: '#professions' },
-        { name: 'Education', url: '#education' },
-        { name: 'Involvements', url: '#involvments' },
-        { name: 'Projects', url: '#projects' },
-        { name: 'Skills', url: '#skills' },
-    ]
-
     const socials: { name: string, url: string, image: string }[] = [
         { name: 'Gmail', url: 'mailto:erikpersson0884@gmail.com', image: gmail },
         { name: 'LinkedIn', url: 'https://linkedin.com/in/erikpersson0884', image: linkedin },
@@ -27,16 +19,8 @@ const Sidebar: FC = () => {
             <a href="/">
                 <img src={logo} className="logo" alt="site logo" />
             </a>
-
-            <nav>
-                <ul className='table-of-contents no-list-style'>
-                    {links.map((link) => (
-                        <li key={link.name}>
-                            <a className='no-link-style link-underline' href={link.url}>{link.name}</a>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
+            
+            <p className='link-underline'>Contact</p>
 
             <ul className='socials no-list-style'>
                 {socials.map((social) => (
